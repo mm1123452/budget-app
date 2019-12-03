10405344cbfee31cc6c194507d5bdefa894982da
Mabel <macmota@hotmail.com>
2019-12-02 20:29:40 -0500

    setting up routes

--- src/server/main.js
+++ src/server/main.js
@@ -9,8 +9,12 @@ const PORT = process.env.PORT || 5000;
 //db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
 
+app.use('/api/users', require('./routes/users'))
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/budget', require('./routes/budget'))
+
 app.get('/', (req,res) => {
-	res.send('MAIN')
+	res.json({msg:'Hello World'})
 })
 
 app.listen(PORT, () => console.log(`app started on port ${PORT}`))
---
+++ src/server/routes/budget.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const router = express.Router();
+
+// @route   GET api/budget
+// @desc    Get all user's budget
+// @access  Private
+router.get('/', (req,res) => {
+	res.send('Get all contacts')
+});
+
+// @route   POST api/budget
+// @desc    Add a new budget 
+// @access  Private
+router.post('/', (req,res) => {
+	res.send('Add budget')
+});
+
+
+// @route   PUT api/budget/:id
+// @desc    Update a budget
+// @access  Private
+router.put('/:id', (req,res) => {
+	res.send('Update budget')
+});
+
+
+// @route   Delete api/budget/:id
+// @desc    Delete a budget
+// @access  Private
+router.delete('/:id', (req,res) => {
+	res.send('Delete budget')
+});
+
+module.exports = router;
+
+
+
