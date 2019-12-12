59d80e4624e53a23235d8e2d8a23f541f0f91611
Mabel <macmota@hotmail.com>
2019-12-11 20:29:06 -0500

    implementing json web token

--- src/server/routes/auth.js
+++ src/server/routes/auth.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/User')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const config = require('config')
+const { check, validationResult} = require('express-validator');
 
 // @route   GET api/auth
 // @desc    Get logged in user
