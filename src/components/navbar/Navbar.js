0b1cfe19ad13b567843665e862a152e0ba11df10
Mabel <macmota@hotmail.com>
2019-12-17 20:39:07 -0500

    updating navbar component with icon

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import './Navbar.css'
 
 const Navbar  = () => (
 	<nav className="navbar navbar-dark bg-dark">
-	  <span className="navbar-brand mb-0 h1">Budget App</span>
+		<i className="fas fa-chart-pie fa-2x icon"></i>
+		<span className="navbar-brand mb-0 h1">Budget App</span>
 	</nav>
 )
 
